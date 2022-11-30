cd7e0711680b8b22c94140f6360bf37ef7e5217d
meron <meronayele158@gmail.com>
2022-11-29 22:26:47 -0500

    Netflix

---
+++ src/requests.js
@@ -0,0 +1,17 @@
+const API_KEY = "65de4f0754824e2404b565389839cd15";
+// 65de4f0754824e2404b565389839cd15
+
+// https://api.themoviedb.org/3/discover/tv?api_key=65de4f0754824e2404b565389839cd15&with_networks=213
+
+const requests = {
+	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+	fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
